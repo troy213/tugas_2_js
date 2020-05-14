b6e7510b983efdfbff0bddced7ae6341214bf61d
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2020-05-14 20:28:28 +0700

    Tugas 2 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+var a,b,c,d,e;
+a = 5;
+b = "Variabel String";
+c = true;
+d = null;
+e = ['arr1','arr2','arr3'];
+
+console.log("Number :",a);
+console.log("String :",b);
+console.log("Boolean :",c);
+console.log("Null :",d);
+console.log("Array :",e);
